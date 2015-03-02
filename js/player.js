c8026f6033a1db99854cd4d9f8615debb1224127
Alexander Allen <aallen@blazinweb.com>
2015-03-01 22:10:22 -0600

    some layout

--- js/player.js
+++ js/player.js
@@ -1,13 +1,6 @@
 ;(function(){
     window.d = jsnox(React);
 
-    // var searchText = 'Kenny Chesney'
-    
-    // $.get(url).then(function(data){
-    //     console.log(data)
-    //     // debugger
-    // });
-
     Backbone.Router.extend({
 
         initialize:function(){
@@ -93,71 +86,57 @@
             
             var model = this.props.results[e.currentTarget.id.toString().replace(/[a-z]/gi,'')]
             
-            player.loadTrack(model.id)
-            var thumbNode = this.refs['thumb'+model.id].getDOMNode()
-            var audioNode = this.refs['track'+model.id].getDOMNode()
+            // player.loadTrack(model.id)
+            var playerNode = this.refs['player'+model.id].getDOMNode()
+            var audioNode  = this.refs['track'+model.id].getDOMNode()
 
-            $('.controls').addClass('hidden').attr('src','')
-            $('.thumbs').addClass('hidden')
-            $(thumbNode).removeClass('hidden')
-            $(audioNode).removeClass('hidden')
+            $('.controls').attr('src','');
+            $('.player').addClass('hidden');
+            $(playerNode).removeClass('hidden');
 
             audioNode.src = ['http://api.soundcloud.com/tracks/',
                 model.id,
                 '/stream' ,
-                '?client_id=f88b5e0c067373ed7a07c3a0a3e4d588'].join('')
-            // debugger
-            // $.get(model.stream_url+'?client_id=f88b5e0c067373ed7a07c3a0a3e4d588')
-            // debugger
-
+                '?client_id=f88b5e0c067373ed7a07c3a0a3e4d588'].join('');
+            
         },
         render: function() {
             var self = this;
-            return d('ul', null, this.props.results.map(function(track , index) {
-                return d('li#index'+index, {onClick:self._selectTrack,key:track.id}, [
-                    d('div', {key:'userName'+track.id},'author:'+track.user.username),
-                    d('div', {key:'title'+track.id},'title:'+track.title),
 
-                    d('div', {key:'played'+track.id},'played:'+track.playback_count),
-                    d('div', {key:'favorited'+track.id},'favorited:'+track.favoritings_count),
+            var listItems = this.props.results.map(function(track , index) {
+                    
+                return  d('li#index'+index , {onClick:self._selectTrack,key:'userName' + track.id} , track.user.username)
+                        
 
-                    // d('div' + track.id , track.artwork_url),
-                    d('img[src='+track.artwork_url+'].thumbs.hidden@thumb' + track.id),
-                    d('audio[controls=true].controls.hidden@track' + track.id)
-                ]
-                );
-            }));
+
+            })
+            var players = this.props.results.map(function(track , index) {
+
+                return d('div.player.hidden@player' + track.id , [
+                    d('h3', {key:'userName' + track.id} ,  track.user.username) ,
+                    d('img[src=' + (track.artwork_url||'./pics/notfound.jpg') + '].thumbs') ,
+
+                    d('audio[controls=true].controls@track' + track.id),
+
+                    d('div', {key:'title' + track.id} , track.title),
+
+                    d('span[title='+track.playback_count+']', {key:'played' + track.id},'played') ,
+                    d('span[title='+track.favoritings_count+']', {key:'favorited' + track.id} , 'Fav\'ed')
+
+                ]);
+
+            });
+
+            return d('div', null, [d('ul.grid.grid-4-600.grid-2-400' , null , listItems) , players]);
         }
     });
 
-    // "playback_count": 10,
-    // "download_count": 6,
-    // "favoritings_count": 1,
-
     var searcher = React.createClass({
        
-        // // creates the state object when a component is first created
         getInitialState: function() {
             return { results: []}
         },
 
-        // // called when a component is attached to the DOM
-        // componentDidMount: function(data) {
-        //     // debugger;
-        // },
-        // // called when a component is detached from the DOM
-        // componentWillUnmount: function() {
-        //     // debugger;
-        // },
-        _showResults:function() {
-
-            // var 
-            // $(this.refs.results.getDOMNode()).html(function(){
-
-
-            // })
-
-        },
         _submit:function(e){
     
             e.preventDefault()
@@ -165,63 +144,29 @@
                                 .addClass('top');
 
             var self = this;
-            this.props.model.setURL( { searchText:searchbox.val() } )
+            this.props.model.setURL( { searchText:searchbox.val() , limit:12 } )
                 .fetch()
                 .then(function(data) {
-                    self.setState({results:data})
-                    // debugger
-                    // self._showResults()
+                    
+                    self.setState({results:data});
+                    
                 });
         },
 
-        // called whenever the state changes
         render: function() {
 
             return d('form' , {onSubmit: this._submit} , [
 
-                    d('input[type=text].search.centerit@searchbox' , {placeholder:'search'}) ,
-                    d(searchResults, {results: this.state.results})
-                    // d('div.results@results')
-                ]
-            );
+                d('input[type=text].search.centerit@searchbox' , {placeholder:'search'}) ,
+                
+                d(searchResults, {results: this.state.results})
+            ]);
 
         }
     });
 
 
-// var context = new webkitAudioContext(),
-//     audio = new Audio(),
-//     source,
-//     // `stream_url` you'd get from 
-//     // requesting http://api.soundcloud.com/tracks/6981096.json
-//     url = 'http://api.soundcloud.com/tracks/6981096/stream' +
-//           '?client_id=f88b5e0c067373ed7a07c3a0a3e4d588';
-
-// audio.src = url;
-// audio.controls = true
-// source = context.createMediaElementSource(audio);
-// source.connect(context.destination);
-// source.mediaElement.play();
-
-
-
-    // SC.initialize({
-    //     client_id: "f88b5e0c067373ed7a07c3a0a3e4d588",
-    //     redirect_uri: "./callback.html",
-    // });
-
-    // SC.connect(function(){
-    //     SC.put("/me/followings/3207", function(user, error){
-    //         if(error){
-    //             alert("Error: " + error.message);
-    //         }else{
-    //             alert("You are now following " + user.username);
-    //         }
-    //     });
-    // });
     var search = new searched
-    // var player = new player
-
     
     var el = React.createElement(searcher, {model:search});
     React.render(el, $('.app-container')[0]);
